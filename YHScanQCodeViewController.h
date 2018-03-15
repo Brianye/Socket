7479aed717d848b036e3cfa947d87770ad27670c
yehui1 <601@hulubao.com>
2018-03-15 21:37:31 +0800

    “提交信息”

---
+++ YHScanQCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  YHScanQCodeViewController.h
+//  taiyingcang
+//
+//  Created by hulubao on 2018/1/16.
+//  Copyright © 2018年 hulubao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YHScanQCodeViewController : UIViewController
+
+@end
